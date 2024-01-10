8caf9a748e9ebe954de2430a6ff4386c03d40172
Nurseda Diker <54947744+nurseda-diker@users.noreply.github.com>
2024-01-10 17:23:15 +0300

    test first development

---
+++ unit-test/TestFirstDevelopment/Demo.Tests/GruplayiciTests.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace Demo.Tests
+{
+    [TestClass]
+    public class GruplayiciTests
+    {
+        private List<Olcum> OlcumListesiOlustur(int adet)
+        {
+            var olcumler=new List<Olcum>();
+            for (int i = 0; i < adet; i++)
+            {
+                olcumler.Add(new Olcum
+                {
+                    EnYuksek = 10,
+                    EnDusuk = 1
+                });
+            }
+            return olcumler;
+        }
+
+
+
+
+        [TestMethod]
+        public void bir_elemanli_liste_birerli_gruplanmak_istediginde_grup_sayisi_bir_olmalidir()
+        {
+            var olcumler = OlcumListesiOlustur(1);
+
+            var gruplayici = new Gruplayici(1);
+            var gruplar = gruplayici.Grupla(olcumler);
+
+            Assert.AreEqual(1,gruplar.Count);
+
+
+
+        }
+
+
+        [TestMethod]
+        public void alti_elemanli_liste_ikişerli_gruplanmak_istediginde_grup_sayisi_uc_olmalidir()
+        {
+            var olcumler = OlcumListesiOlustur(6);
+
+            var gruplayici = new Gruplayici(2);
+            var gruplar = gruplayici.Grupla(olcumler);
+
+            Assert.AreEqual(3, gruplar.Count);
+
+
+        }
+
+        [TestMethod]
+        public void elli_elemanli_liste_onarli_gruplanmak_istediginde_grup_sayisi_bes_olmalidir()
+        {
+            var olcumler = OlcumListesiOlustur(50);
+
+            var gruplayici = new Gruplayici(10);
+            var gruplar = gruplayici.Grupla(olcumler);
+
+            Assert.AreEqual(5, gruplar.Count);
+
+
+        }
+
+
+    }
+}
---
+++ unit-test/TestFirstDevelopment/Demo/Olcum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    public class Olcum
+    {
+        public int EnDusuk { get; set; }
+        public int EnYuksek { get; set; }
+    }
+}
